b2038fb52320e75a70f8087ac2b9f95adbc031ae
Trupti~ <truppawar2801@gmail.com>
2023-05-22 23:52:54 +0530

    Creating first Component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -14,23 +14,22 @@ const ExpenseItem = (props) => {
     console.log(title);
   };
 
-  const [amount, setAmount] = useState(props.amount);
+  // const [amount, setAmount] = useState(props.amount);
 
-  const clickHandle = () => {
-    setAmount('100$');
-    console.log(amount);
-  }
+  // const clickHandle = () => {
+  //   setAmount('100$');
+  //   console.log(amount);
+  // }
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
-        
         <div className='expense-item__price'>${props.amount}</div>
-        <h2>{amount}</h2>
+        {/* <h2>{amount}</h2> */}
       </div>
-      <button onClick={clickHandle}>Change Amount</button>
+      {/* <button onClick={clickHandle}>Change Amount</button> */}
       <button onClick={clickHandler}>Change Title</button>
       
     </Card>
